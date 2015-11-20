e3216c39498f387007cbb2b0f8700fb16637aae7
avdrobotov <avdrobotov@gmail.com>
2015-11-20 15:24:47 +0300

    BeginTest

---
+++ TestWebServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebServer.Startup))]
+namespace TestWebServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
